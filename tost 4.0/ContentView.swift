f00166119e632ab8295c2ed16cae1205c53e6e22
sidnevi <ilartft@gmail.com>
2026-03-18 01:06:31 +0300

    Initial Commit

---
+++ tost 4.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tost 4.0
+//
+//  Created by i sidnev on 18.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tost 4.0/tost_4_0App.swift
@@ -0,0 +1,17 @@
+//
+//  tost_4_0App.swift
+//  tost 4.0
+//
+//  Created by i sidnev on 18.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct tost_4_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
